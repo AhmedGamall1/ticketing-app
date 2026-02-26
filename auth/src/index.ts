d541026417beebf2c36282848d2c5f2062b4fe69
ahmed <ag3204123@gmail.com>
2026-02-26 13:41:20 -0800

    adding ingress load-balancer

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("ok");
+});
+
 app.listen(3000, () => {
   console.log("Auth service is running on port 3000");
 });
