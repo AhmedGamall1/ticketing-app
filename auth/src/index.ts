9df264237e599df00529d37036f1444f0348bafd
ahmed <ag3204123@gmail.com>
2026-02-27 03:49:29 -0800

    adding user routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user.js";
+import { signoutRouter } from "./routes/signout.js";
+import { signinRouter } from "./routes/signin.js";
+import { signupRouter } from "./routes/signup.js";
 
 const app = express();
 
+// middlewares
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("ok");
-});
+// routes
+app.use(currentUserRouter);
+app.use(signoutRouter);
+app.use(signinRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Auth service is running on port 3000");
