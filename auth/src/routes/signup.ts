4f3f3465083947c9ba3d4b8c23310ae7117116f5
ahmed <ag3204123@gmail.com>
2026-03-01 16:34:11 -0800

    handle validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+import { body, validationResult } from "express-validator";
+
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("ok");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    console.log("Creating a user");
+
+    res.send("ok");
+  },
+);
 
 export { router as signupRouter };
