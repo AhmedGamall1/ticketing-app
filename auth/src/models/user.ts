c3425779bf0458ed66e8aa789e549ee5a3bbfbc4
ahmed <ag3204123@gmail.com>
2026-03-05 04:45:47 -0800

    adding user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Adding a static method to the user schema for building a new user
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
