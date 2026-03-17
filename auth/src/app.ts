d7b3121e2e5f04530381cf67fdf61da4572a8b83
ahmed <ag3204123@gmail.com>
2026-03-17 08:28:42 -0700

    index to app refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { currentUserRouter } from "./routes/current-user.js";
+import { signoutRouter } from "./routes/signout.js";
+import { signinRouter } from "./routes/signin.js";
+import { signupRouter } from "./routes/signup.js";
+import { errorHandler } from "./middlewares/error-handler.js";
+import { NotFoundError } from "./errors/not-found-error.js";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set("trust proxy", true);
+
+// middlewares
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
+
+// routes
+app.use(currentUserRouter);
+app.use(signoutRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+
+// undefined route handler
+app.use((req, res, next) => {
+  throw new NotFoundError();
+});
+
+// error handler middleware
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from "express";
-import { currentUserRouter } from "./routes/current-user.js";
-import { signoutRouter } from "./routes/signout.js";
-import { signinRouter } from "./routes/signin.js";
-import { signupRouter } from "./routes/signup.js";
-import { errorHandler } from "./middlewares/error-handler.js";
-import { NotFoundError } from "./errors/not-found-error.js";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set("trust proxy", true);
-
-// middlewares
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  }),
-);
-
-// routes
-app.use(currentUserRouter);
-app.use(signoutRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-
-// undefined route handler
-app.use((req, res, next) => {
-  throw new NotFoundError();
-});
-
-// error handler middleware
-app.use(errorHandler);
+import { app } from "./app.js";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
