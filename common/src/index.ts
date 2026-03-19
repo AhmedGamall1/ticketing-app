76014ffc20ad09645882a42662f3718246ab3bd0
ahmed <ag3204123@gmail.com>
2026-03-18 19:18:03 -0700

    additional configs

---
+++ common/src/index.ts
@@ -0,0 +1,17 @@
+interface Test {
+  t1: Number;
+  t2: String;
+}
+
+const test: Test = {
+  t1: 1,
+  t2: "test",
+};
+
+const t2est: Test = {
+  t1: 1,
+  t2: "test",
+};
+
+console.log(test);
+console.log(t2est);
