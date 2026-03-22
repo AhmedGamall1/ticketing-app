16022393c7bc11d0c9a436beda53993858371d08
ahmed <ag3204123@gmail.com>
2026-03-22 07:32:26 -0700

    relocating shared code

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,19 +1,13 @@
-interface Test {
-  t1: Number;
-  t2: String;
-}
+// Re-exporting all the errors from the errors directory
+export * from "./errors/bad-request-error.js";
+export * from "./errors/custom-error.js";
+export * from "./errors/database-connection-error.js";
+export * from "./errors/not-found-error.js";
+export * from "./errors/request-validation-error.js";
+export * from "./errors/not-authorized-error.js";
 
-const test: Test = {
-  t1: 1,
-  t2: "test",
-};
-
-const t2est: Test = {
-  t1: 1,
-  t2: "test",
-};
-
-console.log(test);
-console.log(t2est);
-
-export default test;
+// Re-exporting all the middlewares from the middlewares directory
+export * from "./middlewares/current-user.js";
+export * from "./middlewares/error-handler.js";
+export * from "./middlewares/require-auth.js";
+export * from "./middlewares/validate-request.js";
